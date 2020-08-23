f0eeb1ea7b814239694f3ad3d6e0adf0a6dc779b
Michael Turchin <mturchin20@gmail.com>
2020-08-23 15:32:32 -0400

    Working towards first vignette [documenting '/data' components]

--- R/data.R
+++ R/data.R
@@ -6,28 +6,20 @@ MAPITR_SimData_Phenotype.rda
 MAPITR_SimData_Pathways.rda
 
 
-#' @name bmass_TestData1
+#' @name MAPITR_TestData1_Genotypes
 #'
-#' @title bmass Test Dataset 1
+#' @title MAPITR TestData1 Genotypes
 #'
 #' @docType data
 #'
-#' @description A manually created sample dataset for use in unit
+#' @description A simulated dataset of genotypes for use in unit
 #' tests.
 #'
-#' @format A data frame with 11 rows and 9 variables:
+#' @format A data frame with 750 rows and 10000 variables (SNPs):
 #'
 #' \describe{
-#'   \item{Chr}{chromosome}
-#'   \item{BP}{basepair position}
-#'   \item{Marker}{rsID# or other identifier}
-#'   \item{MAF}{Minor Allele Frequency}
-#'   \item{A1}{reference allele}
-#'   \item{A2}{alternative allele}
-#'   \item{Direction}{direction of association
-#'      effect size, + or -}
-#'   \item{pValue}{p-Value of GWAS association}
-#'   \item{N}{sample size}
+#'   \item{SNPs}{SNP genotype, 0,
+#'      1, or 2}
 #' }
 #'
 #' @source Manually created
@@ -36,55 +28,20 @@ MAPITR_SimData_Pathways.rda
 #'
 NULL
 
-#' @name bmass_TestData2
+#' @name MAPITR_TestData1_Phenotype
 #'
-#' @title bmass Test Dataset 2
+#' @title MAPITR TestData1 Phenotype
 #'
 #' @docType data
 #'
-#' @description A manually created sample dataset for use in unit
+#' @description A simulated list of phenotypes for use in unit
 #' tests.
 #'
-#' @format A data frame with 11 rows and 9 variables:
+#' @format A vector with 750 rows and 1 variables:
 #'
 #' \describe{
-#'   \item{Chr}{chromosome}
-#'   \item{BP}{basepair position}
-#'   \item{Marker}{rsID# or other identifier}
-#'   \item{MAF}{Minor Allele Frequency}
-#'   \item{A1}{reference allele}
-#'   \item{A2}{alternative allele}
-#'   \item{Direction}{direction of association
-#'      effect size, + or -}
-#'   \item{pValue}{p-Value of GWAS association}
-#'   \item{N}{sample size}
-#' }
-#'
-#' @keywords data
-NULL
-
-#' @name bmass_SimulatedData1
-#'
-#' @title bmass Simulated Dataset 1
-#'
-#' @docType data
-#'
-#' @description A manually created sample dataset for use in Roxygen2
-#' documents and vignettes.
-#'
-#' @format A data frame with 11 rows and 9 variables:
-#'
-#' \describe{
-#'   \item{Chr}{chromosome}
-#'   \item{BP}{basepair position}
-#'   \item{Marker}{rsID# or other identifier}
-#'   \item{MAF}{Minor Allele Frequency}
-#'   \item{A1}{reference allele}
-#'   \item{A2}{alternative allele}
-#'   \item{Direction}{direction of association
-#'      effect size, + or -}
-#'   \item{pValue}{p-Value of GWAS association}
-#'   \item{N}{sample size}
+#'   \item{Phenotype}{Simulated
+#'      phenotype values}
 #' }
 #'
 #' @source Manually created
@@ -93,4 +50,23 @@ NULL
 #'
 NULL
 
-
+#' @name MAPITR_TestData1_Phenotype
+#'
+#' @title MAPITR TestData1 Phenotype
+#'
+#' @docType data
+#'
+#' @description A simulated list of phenotypes for use in unit
+#' tests.
+#'
+#' @format A vector with 750 rows and 1 variables:
+#'
+#' \describe{
+#'   \item{Phenotype}{Simulated
+#'      phenotype values}
+#' }
+#'
+#' @source Manually created
+#'
+#' @keywords data
+#'
