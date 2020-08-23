fd74162925bf8a669e89d76f6440a480c67bfafd
Michael Turchin <mturchin20@gmail.com>
2020-08-23 11:31:18 -0400

    Working towards first vignette [documenting '/data' components]

---
+++ R/data.R
@@ -0,0 +1,96 @@
+MAPITR_TestData_Genotypes.rda
+MAPITR_TestData_Phenotype.rda
+MAPITR_TestData_Pathways.rda
+MAPITR_SimData_Genotypes.rda
+MAPITR_SimData_Phenotype.rda
+MAPITR_SimData_Pathways.rda
+
+
+#' @name bmass_TestData1
+#'
+#' @title bmass Test Dataset 1
+#'
+#' @docType data
+#'
+#' @description A manually created sample dataset for use in unit
+#' tests.
+#'
+#' @format A data frame with 11 rows and 9 variables:
+#'
+#' \describe{
+#'   \item{Chr}{chromosome}
+#'   \item{BP}{basepair position}
+#'   \item{Marker}{rsID# or other identifier}
+#'   \item{MAF}{Minor Allele Frequency}
+#'   \item{A1}{reference allele}
+#'   \item{A2}{alternative allele}
+#'   \item{Direction}{direction of association
+#'      effect size, + or -}
+#'   \item{pValue}{p-Value of GWAS association}
+#'   \item{N}{sample size}
+#' }
+#'
+#' @source Manually created
+#'
+#' @keywords data
+#'
+NULL
+
+#' @name bmass_TestData2
+#'
+#' @title bmass Test Dataset 2
+#'
+#' @docType data
+#'
+#' @description A manually created sample dataset for use in unit
+#' tests.
+#'
+#' @format A data frame with 11 rows and 9 variables:
+#'
+#' \describe{
+#'   \item{Chr}{chromosome}
+#'   \item{BP}{basepair position}
+#'   \item{Marker}{rsID# or other identifier}
+#'   \item{MAF}{Minor Allele Frequency}
+#'   \item{A1}{reference allele}
+#'   \item{A2}{alternative allele}
+#'   \item{Direction}{direction of association
+#'      effect size, + or -}
+#'   \item{pValue}{p-Value of GWAS association}
+#'   \item{N}{sample size}
+#' }
+#'
+#' @keywords data
+NULL
+
+#' @name bmass_SimulatedData1
+#'
+#' @title bmass Simulated Dataset 1
+#'
+#' @docType data
+#'
+#' @description A manually created sample dataset for use in Roxygen2
+#' documents and vignettes.
+#'
+#' @format A data frame with 11 rows and 9 variables:
+#'
+#' \describe{
+#'   \item{Chr}{chromosome}
+#'   \item{BP}{basepair position}
+#'   \item{Marker}{rsID# or other identifier}
+#'   \item{MAF}{Minor Allele Frequency}
+#'   \item{A1}{reference allele}
+#'   \item{A2}{alternative allele}
+#'   \item{Direction}{direction of association
+#'      effect size, + or -}
+#'   \item{pValue}{p-Value of GWAS association}
+#'   \item{N}{sample size}
+#' }
+#'
+#' @source Manually created
+#'
+#' @keywords data
+#'
+NULL
+
+
